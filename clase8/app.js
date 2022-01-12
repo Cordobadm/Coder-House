20796eb09d6a02dc69dd76995ab5910fcceb7ccb
Diego Cordoba <cordobadiegom@gmail.com>
2022-01-12 10:37:05 -0300

    agregamos prompt para mostrar lista de productos disponibles, y mensaje de despedida

--- clase8/app.js
+++ clase8/app.js
@@ -1,9 +1,9 @@
 class Producto {
   constructor(marca, rubro, nombre, precio) {
-    (this.marca = marca),
-      (this.rubro = rubro),
-      (this.nombre = nombre),
-      (this.precio = precio);
+    this.marca = marca,
+    this.rubro = rubro,
+    this.nombre = nombre,
+    this.precio = precio;
   }
 }
 
@@ -16,16 +16,27 @@ const productos = [
   (producto6 = new Producto("GAMMA", "PEINADO", "SECADOR", 4000)),
 ];
 
-let nombreIngresado = prompt("Ingrese su nombre: ").toUpperCase()
-const saludoBienvenida = document.createElement('h1')
-saludoBienvenida.innerHTML = `Bienvenido/a ${nombreIngresado} a: <p> 'MAGNOLIA SPA'`
-document.body.appendChild(saludoBienvenida)
+let nombreIngresado = prompt("Ingrese su nombre: ").toUpperCase();
 
-const textoProductos = document.createElement('h2')
-textoProductos.innerHTML = 'Productos disponibles'
-document.body.appendChild(textoProductos)
+let entrada = prompt(
+  "¿Desea ver los productos disponibles? Si/No"
+).toUpperCase();
+if (entrada === "SI") {
+  const saludoBienvenida = document.createElement("h1");
+  saludoBienvenida.innerHTML = `Bienvenid@ ${nombreIngresado} a: <p> 'MAGNOLIA SPA'`;
+  document.body.appendChild(saludoBienvenida);
+  const textoProductos = document.createElement("h2");
+  textoProductos.innerHTML = "Productos disponibles";
+  document.body.appendChild(textoProductos);
 
+  for (const producto of productos) {
+    let listaProductos = document.createElement("div");
+    listaProductos.innerHTML += `Nombre: ${producto.nombre} - Marca: ${producto.marca} - Precio: $${producto.precio} - Rubro: ${producto.rubro} <p>`;
 
-for (const producto of productos) {
-     
+    document.body.appendChild(listaProductos);
+  }
+} else {
+  const saludoDespedida = document.createElement("h1");
+  saludoDespedida.innerHTML = `Gracias ${nombreIngresado} por visitarnos. Te esperamos pronto`;
+  document.body.appendChild(saludoDespedida);
 }
