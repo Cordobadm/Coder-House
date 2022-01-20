c921fb9403009973e8e9c63b94d92b50764f8ea7
Diego Cordoba <cordobadiegom@gmail.com>
2022-01-20 15:23:50 -0300

    arreglo de class

--- proyectoCoder/js/components/class.js
+++ proyectoCoder/js/components/class.js
@@ -10,15 +10,15 @@ class Persona {
     twitter,
     informacion
   ) {
-    (this.nombre = nombre),
-      (this.apellido = apellido),
-      (this.cargo = cargo),
-      (this.funcion = funcion);
-    this.imagen = imagen;
-    (this.instagram = instagram),
-      (this.facebook = facebook),
-      (this.twitter = twitter);
-    this.informacion = informacion;
+    this.nombre = nombre,
+    this.apellido = apellido,
+    this.cargo = cargo,
+    this.funcion = funcion,
+    this.imagen = imagen,
+    this.instagram = instagram,
+    this.facebook = facebook,
+    this.twitter = twitter,
+    this.informacion = informacion
   }
 }
 
@@ -32,12 +32,12 @@ class Producto {
     precioPorUnidad,
     img
   ) {
-    (this.id = id),
-      (this.marca = marca),
-      (this.rubro = rubro),
-      (this.nombre = nombre),
-      (this.precio = precio),
-      (this.precioPorUnidad = precioPorUnidad),
-      (this.img = img)
+    this.id = id,
+    this.marca = marca,
+    this.rubro = rubro,
+    this.nombre = nombre,
+    this.precio = precio,
+    this.precioPorUnidad = precioPorUnidad,
+    this.img = img
   }
 }
